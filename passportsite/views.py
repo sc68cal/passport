badb748dc64aa5163822f0f9cb849e7546199e06
Sean M. Collins <sean@coreitpro.com>
2010-08-22 23:08:56 +0000

    filter to only get published

--- passportsite/views.py
+++ passportsite/views.py
@@ -4,7 +4,7 @@ from pages.models import Page
 
 def index_view(request):
     articles = []
-    for pg in Page.objects.all():
+    for pg in Page.objects.filter(status=1):
             if not pg.is_child_node() and pg.get_children():
                 articles.append(pg.get_date_ordered_children_for_frontend()[0])
     print articles
