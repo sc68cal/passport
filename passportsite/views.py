54cc23c299c363fbd3564927feee0886097b6016
Sean M. Collins <sean@coreitpro.com>
2010-08-22 23:28:33 +0000

    Fix for corner case when category has only one article.

--- passportsite/views.py
+++ passportsite/views.py
@@ -6,6 +6,10 @@ def index_view(request):
     articles = []
     for pg in Page.objects.filter(status=1):
             if not pg.is_child_node() and pg.get_children():
-                articles.append(pg.get_date_ordered_children_for_frontend()[0])
+                tmp = pg.get_date_ordered_children_for_frontend()
+                if type(tmp) == type([]):
+                    articles.append(pg.get_date_ordered_children_for_frontend()[0])
+                else:
+                    articles.append(pg.get_date_ordered_children_for_frontend())
     print articles
     return render_to_response('passport/index.html',{'article_list':articles})
