8fe3abed37aad5bd86573160fdc215ef576c4b1b
Sean M. Collins <sean@coreitpro.com>
2010-08-22 23:49:37 +0000

    Fix for case where article had only unpublished children

--- passportsite/views.py
+++ passportsite/views.py
@@ -5,6 +5,8 @@ from pages.models import Page
 def index_view(request):
     articles = []
     for pg in Page.objects.filter(status=1):
-            if not pg.is_child_node() and pg.get_children() > 1:
-                articles.append(pg.get_date_ordered_children_for_frontend()[0])
+            if not pg.is_child_node():
+                tmp = pg.get_date_ordered_children_for_frontend()
+                if tmp:
+                    articles.append(tmp[0])
     return render_to_response('passport/index.html',{'article_list':articles})
